b97cb0510962fffde426b06f5ab770bf7d494d9c
Box CI <53619895+boxci@users.noreply.github.com>
2020-03-05 23:38:11 +0000

    WIP

--- scripts/test-task.js
+++ scripts/test-task.js
@@ -1,8 +1,8 @@
-console.log('hello')
+console.log('hello 1\n\nhello 1\nhello 1\n\nhello 1')
 setTimeout(() => {
-  console.log('hello')
+  console.log('hello 2\n\nhello 2\nhello 2\n\nhello 2')
 
   setTimeout(() => {
-    console.log('hello')
+    console.log('hello 3\n\nhello 3\nhello 3\n\nhello 3')
   }, 5000)
 }, 5000)
