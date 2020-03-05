4c169aa03377a242d9065960a8388d6021c16691
Box CI <53619895+boxci@users.noreply.github.com>
2020-03-05 15:12:59 +0000

    make test-task run for about 10s

--- scripts/test-task.js
+++ scripts/test-task.js
@@ -1,1 +1,7 @@
 console.log('hello\n\nhello\nhello\n\nhello\n\nhello')
+setTimeout(() => {
+  console.log('hello\n\nhello\nhello\n\nhello\n\nhello')
+}, 5000)
+setTimeout(() => {
+  console.log('hello\n\nhello\nhello\n\nhello\n\nhello')
+}, 5000)
